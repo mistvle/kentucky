6edd81a649a5f0b06dcfaac80822d3b82a3c3d08
mistle-official <mistle.official1@gmail.com>
2026-05-24 05:59:57 +0000

    A

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,6 +6,113 @@ module.exports = {
   async execute(client, message) {
     if (message.author.bot) return;
 
+    // ================= KEYWORD EMBEDS =================
+    const content = message.content.toLowerCase();
+
+    if (content.includes("staff")) {
+
+      await message.channel.send({
+        "flags": 32768,
+        "components": [
+          {
+            "type": 17,
+            "components": [
+              {
+                "type": 10,
+                "content": "**Staff Application**"
+              },
+              {
+                "type": 10,
+                "content": "You can apply to become staff at **Kentucky State Roleplay** using the link below. We offer many benefits for our staff members, along with an incredible High Rank Team & welcoming staff experience."
+              },
+              {
+                "type": 14,
+                "divider": false
+              },
+              {
+                "type": 1,
+                "components": [
+                  {
+                    "type": 2,
+                    "style": 5,
+                    "label": "Staff Application",
+                    "url": "https://melonly.xyz/forms/7237583774488203264"
+                  }
+                ]
+              },
+              {
+                "type": 14,
+                "spacing": 2
+              },
+              {
+                "type": 12,
+                "items": [
+                  {
+                    "media": {
+                      "url": "https://cdn.discordapp.com/attachments/1505376044474040440/1505728623922122795/Footers_55.png?ex=6a12eecf&is=6a119d4f&hm=a9ec55994346800496d8b9dc8bdfd398afe93f8cc1bf25bdc0d50dc6d675ef95"
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      });
+
+    }
+
+    if (content.includes("appeal")) {
+
+      await message.channel.send({
+        "flags": 32768,
+        "components": [
+          {
+            "type": 17,
+            "components": [
+              {
+                "type": 10,
+                "content": "**Moderation Appeal**"
+              },
+              {
+                "type": 10,
+                "content": "You can appeal your recent in-game moderation using the link provided below. Ensure to submit all necessary information & put as much detail into your appeal as possible to increase the chances of your appeal being accepted."
+              },
+              {
+                "type": 14,
+                "divider": false
+              },
+              {
+                "type": 1,
+                "components": [
+                  {
+                    "type": 2,
+                    "style": 5,
+                    "label": "Moderation Appeal",
+                    "url": "https://melonly.xyz/forms/7319528804647440384"
+                  }
+                ]
+              },
+              {
+                "type": 14,
+                "spacing": 2
+              },
+              {
+                "type": 12,
+                "items": [
+                  {
+                    "media": {
+                      "url": "https://cdn.discordapp.com/attachments/1505376044474040440/1505728623922122795/Footers_55.png?ex=6a12eecf&is=6a119d4f&hm=a9ec55994346800496d8b9dc8bdfd398afe93f8cc1bf25bdc0d50dc6d675ef95"
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      });
+
+    }
+
     // ================= AFK CHECK =================
     let mentionedUser = message.mentions.users.first();
 
